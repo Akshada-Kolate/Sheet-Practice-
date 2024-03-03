42dfa5d1a814b7087eb23bd4fb9d07ab2f7ead09
Akshada Kolate <90676596+Akshada-Kolate@users.noreply.github.com>
2024-03-03 17:10:36 +0530

    Create 1.java

---
+++ 1. Basics/1. Patterns/1.java
@@ -0,0 +1,14 @@
+import java.lang.*;
+import java.util.*;
+
+public class Pattern {
+    public static void main(String[] args){
+        int r=5,c=5;
+        for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++){
+                System.out.print("* ");
+            }
+            System.out.println("");
+        }
+    }
+}
